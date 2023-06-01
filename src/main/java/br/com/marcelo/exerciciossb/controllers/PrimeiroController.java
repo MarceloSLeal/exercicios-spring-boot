56f401332dc52dec5f4062ac23a77426ca8bfa0d
MarceloSLeal <marcelosleal@outlook.com>
2023-06-01 20:33:44 -0300

    Exemplo super simples para criar um endpoint e retornar uma string

---
+++ src/main/java/br/com/marcelo/exerciciossb/controllers/PrimeiroController.java
@@ -0,0 +1,18 @@
+package br.com.marcelo.exerciciossb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class PrimeiroController {
+
+
+    @GetMapping("/ola")
+    public String ola() {
+        return "Olá Spring Boot!";
+    }
+
+}
