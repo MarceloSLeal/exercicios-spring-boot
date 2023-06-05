f711c5fd96996b15e1d9858d11b748eeaf03046c
MarceloSLeal <marcelosleal@outlook.com>
2023-06-05 18:17:38 -0300

    Passando parâmetros para as requisiçõe GET, recebendo no método do controller e retornando um valor

--- src/main/java/br/com/marcelo/exerciciossb/controllers/ClienteController.java
+++ src/main/java/br/com/marcelo/exerciciossb/controllers/ClienteController.java
@@ -2,6 +2,7 @@ package br.com.marcelo.exerciciossb.controllers;
 
 import br.com.marcelo.exerciciossb.models.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ public class ClienteController {
     @GetMapping("/qualquer")
     public Cliente obterCliente() {
         return new Cliente(28, "Pedro", "123.456.789-00");
+    }
+
+    @GetMapping("/{id}")
+    public Cliente obterClientePorId1(@PathVariable int id) {
+        return new Cliente(id, "Maria", "987.654.321-00");
     }
 
 }
