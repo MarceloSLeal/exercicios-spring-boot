717e5c9d832c3a94ce6aec24bea60b5734d93010
MarceloSLeal <marcelosleal@outlook.com>
2023-06-05 20:30:35 -0300

    Passando parâmetros pela url e tratando no método

--- src/main/java/br/com/marcelo/exerciciossb/controllers/ClienteController.java
+++ src/main/java/br/com/marcelo/exerciciossb/controllers/ClienteController.java
@@ -1,10 +1,7 @@
 package br.com.marcelo.exerciciossb.controllers;
 
 import br.com.marcelo.exerciciossb.models.Cliente;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clientes")
@@ -18,6 +15,11 @@ public class ClienteController {
     @GetMapping("/{id}")
     public Cliente obterClientePorId1(@PathVariable int id) {
         return new Cliente(id, "Maria", "987.654.321-00");
+    }
+
+    @GetMapping
+    public Cliente obterClientePorId2(@RequestParam(name = "id", defaultValue = "1") int id) {
+        return new Cliente(id, "João Augusto", "111.222.333.-44");
     }
 
 }
