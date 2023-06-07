966287fee59b0b1e82c68adf33ce3be8c23e74ac
MarceloSLeal <marcelosleal@outlook.com>
2023-06-07 00:32:56 -0300

    Simplificando o código

--- src/main/java/br/com/marcelo/exerciciossb/controllers/CalculadoraController.java
+++ src/main/java/br/com/marcelo/exerciciossb/controllers/CalculadoraController.java
@@ -1,30 +1,25 @@
 package br.com.marcelo.exerciciossb.controllers;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/calculadora")
 public class CalculadoraController {
 
     // DESAFIO criar os seguintes endpoints recebendo parâmetros e retornando o resultado
     // /calculadora/somar/10/20
     // /calculadora/subtrair?a=10&b=20
 
-    // http://localhost:8080/somar?a=10&b=20
-    @GetMapping("/somar")
-    public String somar(@RequestParam String a, @RequestParam String b) {
-        int c = Integer.parseInt(a) + Integer.parseInt(b);
-        return String.valueOf(c);
+    // http://localhost:8080/calculadora/somar/10/10
+    @GetMapping("/somar/{a}/{b}")
+    public int somar(@PathVariable int a, @PathVariable int b) {
+        return a + b;
     }
 
-    // http://localhost:8080/subtrair?a=50&b=20
+    // http://localhost:8080/calculadora/subtrair?a=40&b=20
     @GetMapping("/subtrair")
-    public String subtrair(@RequestParam String a, @RequestParam String b) {
-        int c = Integer.parseInt(a) - Integer.parseInt(b);
-        return String.valueOf(c);
+    public int subtrair(@RequestParam(name = "a") int a, @RequestParam(name = "b") int b) {
+        return a - b;
     }
 
 }
