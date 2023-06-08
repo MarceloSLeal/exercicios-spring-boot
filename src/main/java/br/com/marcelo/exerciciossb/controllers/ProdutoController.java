1d03363328a37704b24d066002bfd3eb87e25652
MarceloSLeal <marcelosleal@outlook.com>
2023-06-08 19:50:21 -0300

    Adicionada dependência para fazer validação, na entidade foi adicionado marcações para fazer as validações

--- src/main/java/br/com/marcelo/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/br/com/marcelo/exerciciossb/controllers/ProdutoController.java
@@ -2,6 +2,7 @@ package br.com.marcelo.exerciciossb.controllers;
 
 import br.com.marcelo.exerciciossb.model.entities.Produto;
 import br.com.marcelo.exerciciossb.model.repositories.ProdutoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,7 +14,7 @@ public class ProdutoController {
     private ProdutoRepository produtoRepository;
 
     @PostMapping
-    public @ResponseBody Produto novoProduto(Produto produto) {
+    public @ResponseBody Produto novoProduto(@Valid Produto produto) {
         produtoRepository.save(produto);
         return produto;
     }
--- src/main/java/br/com/marcelo/exerciciossb/model/entities/Produto.java
+++ src/main/java/br/com/marcelo/exerciciossb/model/entities/Produto.java
@@ -1,6 +1,9 @@
 package br.com.marcelo.exerciciossb.model.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "produtos")
@@ -9,8 +12,15 @@ public class Produto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @NotBlank
     private String nome;
+
+    @Min(0)
     private double preco;
+
+    @Min(0)
+    @Max(1)
     private double desconto;
 
     public Produto() {
@@ -42,7 +52,7 @@ public class Produto {
         return preco;
     }
 
-    public void setPreco(int preco) {
+    public void setPreco(double preco) {
         this.preco = preco;
     }
 
